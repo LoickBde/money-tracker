37795c48f1d7915707e25b825a411d6871490bea
LoickBde <loick.baude@gmail.com>
2022-10-02 18:16:52 +0200

    feat(api): set up basic routes and mini db

--- packages/money-tracker-api/src/main.ts
+++ packages/money-tracker-api/src/main.ts
@@ -1,17 +1,87 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import express, { Request, Response } from 'express';
+import DataStore from 'nedb';
 
-import * as express from 'express';
+// db
+const db = new DataStore({ filename: 'packages/money-tracker-api/money-tracker.db' });
+db.loadDatabase();
 
+// server
 const app = express();
+const port = process.env.port || 3333;
+
+app.use(express.json());
 
 app.get('/api', (req, res) => {
     res.send({ message: 'Welcome to money-tracker-api!' });
 });
 
-const port = process.env.port || 3333;
+app.post('/api/expense', (req: Request, res: Response) => {
+    db.insert(req.body, (err: Error, document) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        res.status(201).send(document);
+    });
+});
+
+app.get('/api/expense', (req: Request, res: Response) => {
+    db.find({}, (err: Error, document) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        if (!document.length) {
+            res.sendStatus(404);
+            return;
+        }
+        res.status(200).send(document);
+    });
+});
+
+app.get('/api/expense/:id', (req: Request, res: Response) => {
+    db.find({ _id: req.params.id }, (err: Error, document) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        if (!document.length) {
+            res.sendStatus(404);
+            return;
+        }
+        res.status(200).send(document);
+    });
+});
+
+app.patch('/api/expense/:id', (req: Request, res: Response) => {
+    db.update({ _id: req.params.id }, { $set: { ...req.body } }, {}, (err: Error, numberOfUpdated: number) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        if (!numberOfUpdated) {
+            res.sendStatus(404);
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
+app.delete('/api/expense/:id', (req: Request, res: Response) => {
+    db.remove({ _id: req.params.id }, (err: Error, n: number) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        if (!n) {
+            res.sendStatus(404);
+            return;
+        }
+
+        res.sendStatus(200);
+    });
+});
+
 const server = app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/api`);
 });
