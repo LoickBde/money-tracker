47c25f4aa5144784fa34bffb91383185ed8aec60
LoickBde <loick.baude@gmail.com>
2022-11-02 23:12:14 +0100

    feat(api): set up orm

--- packages/money-tracker-api/src/main.ts
+++ packages/money-tracker-api/src/main.ts
@@ -1,17 +1,11 @@
 import * as dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
-import DataStore from 'nedb';
-import { clientDemo } from './utils/db-config';
+import { getAllUsers } from './utils/db-config';
 
 dotenv.config();
 
-// db
-const db = new DataStore({ filename: 'packages/money-tracker-api/money-tracker.db' });
-db.loadDatabase();
-
-// server
 const app = express();
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 
@@ -19,76 +13,9 @@ app.get('/api', (req, res) => {
     res.send({ message: 'Welcome to money-tracker-api!' });
 });
 
-app.post('/api/expense', (req: Request, res: Response) => {
-    db.insert(req.body, (err: Error, document) => {
-        if (err) {
-            console.error(err);
-            res.sendStatus(400);
-        }
-        res.status(201).send(document);
-    });
-});
-
-app.get('/api/expense', (req: Request, res: Response) => {
-    db.find({}, (err: Error, document) => {
-        if (err) {
-            console.error(err);
-            res.sendStatus(400);
-        }
-        if (!document.length) {
-            res.sendStatus(404);
-            return;
-        }
-        res.status(200).send(document);
-    });
-});
-
-app.get('/api/expense/:id', (req: Request, res: Response) => {
-    db.find({ _id: req.params.id }, (err: Error, document) => {
-        if (err) {
-            console.error(err);
-            res.sendStatus(400);
-        }
-        if (!document.length) {
-            res.sendStatus(404);
-            return;
-        }
-        res.status(200).send(document);
-    });
-});
-
-app.patch('/api/expense/:id', (req: Request, res: Response) => {
-    db.update({ _id: req.params.id }, { $set: { ...req.body } }, {}, (err: Error, numberOfUpdated: number) => {
-        if (err) {
-            console.error(err);
-            res.sendStatus(400);
-        }
-        if (!numberOfUpdated) {
-            res.sendStatus(404);
-            return;
-        }
-        res.sendStatus(200);
-    });
-});
-
-app.delete('/api/expense/:id', (req: Request, res: Response) => {
-    db.remove({ _id: req.params.id }, (err: Error, n: number) => {
-        if (err) {
-            console.error(err);
-            res.sendStatus(400);
-        }
-        if (!n) {
-            res.sendStatus(404);
-            return;
-        }
-
-        res.sendStatus(200);
-    });
-});
-
 const server = app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
 
-clientDemo();
+getAllUsers();
--- packages/money-tracker-api/src/utils/db-config.ts
+++ packages/money-tracker-api/src/utils/db-config.ts
@@ -1,15 +1,37 @@
-import { Client } from 'pg';
+import { Dialect, Sequelize, DataTypes, Model } from 'sequelize';
 
-export async function clientDemo() {
-    const client = new Client({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-    });
-    await client.connect();
-    const now = await client.query('SELECT * from users');
-    console.log(now.rows);
-    await client.end();
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT as Dialect,
+});
+
+class User extends Model {
+    declare id: number;
+    declare name: string;
+    declare email: string;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    { sequelize: sequelize, tableName: 'users', timestamps: false },
+);
+
+export async function getAllUsers() {
+    const users = await User.findAll();
+    console.log(users.every((user) => user instanceof User)); // true
+    console.log('All users:', JSON.stringify(users, null, 2));
 }
